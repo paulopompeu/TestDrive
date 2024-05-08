ed2aeb7eb11c7f5d0081102a81ca1b62b1234522
paulopompeu <pompeutoledo@gmail.com>
2024-05-08 16:29:46 -0500

    Initial Commit

---
+++ TestDrive/TestDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDriveApp.swift
+//  TestDrive
+//
+//  Created by Paulo P T Oliveira on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
